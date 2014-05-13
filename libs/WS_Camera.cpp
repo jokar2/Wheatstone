5f8b8144da64f804c6c0f972f94d1cf1bb34d883
Jonas <studentjonas@gmail.com>
2014-05-13 12:22:43 +0200

    Added debug information and comments in the function WS_InitCamera

--- libs/WS_Camera.cpp
+++ libs/WS_Camera.cpp
@@ -6,26 +6,24 @@
 #include <opencv2/opencv.hpp>
 #include <opencv/highgui.h>
 
+#define DEBUG 1
+#define FPS 15
+#define WIDTH 640
+#define HEIGHT 480
+
 using namespace std;
 using namespace cv;
 
 int WS_InitCamera(RaspiCam_Cv *Camera){
-	int debug=1;
-	if(debug)cout<<"Setting format of output data to CV_8UC1"<<endl;
-	Camera->set(CV_CAP_PROP_FORMAT, CV_8UC1);
-	Camera->set(CV_CAP_PROP_FPS, 15);
-	Camera->set(CV_CAP_PROP_FRAME_WIDTH, 640);
-	Camera->set(CV_CAP_PROP_FRAME_HEIGHT, 480);
-	
-	if(debug)cout<<"Opening camera... "<<Camera->open()<<endl;
-	/*if(!( Camera->open() ) ){
-		if(debug)cout<<"The camera is active"<<endl;
-	}
-	else{
-		cout<<"Error: camera not active"<<endl;
-		return -1;
-	}	*/
-	
+	if(DEBUG)cout<<"Setting format of output data to CV_8UC1"<<endl;
+	Camera->set(CV_CAP_PROP_FORMAT, CV_8UC1); //CV_8UC1 seems to be standard.
+	if(DEBUG)cout<<"Setting FPS to "<<FPS<<endl;
+	Camera->set(CV_CAP_PROP_FPS, FPS);
+	if(DEBUG)cout<<"Setting frame dimensions to "<<WIDTH<<"x"<<HEIGHT<<endl;
+	Camera->set(CV_CAP_PROP_FRAME_WIDTH, WIDTH);
+	Camera->set(CV_CAP_PROP_FRAME_HEIGHT, HEIGHT);
+
+	if(DEBUG)cout<<"Opening camera... "<<Camera->open()<<endl;
+
 	return 0;
 }
-
