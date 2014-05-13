2d22beab3b658363bb75969f3fb88d493d71da28
jokar2 <pi@raspberrypi.(none)>
2014-05-13 10:59:57 +0000

    Edited the setup to make the camera return colored images instead of black and white

--- libs/WS_Camera.cpp
+++ libs/WS_Camera.cpp
@@ -1,8 +1,5 @@
 #include "WS_Camera.h"
-#include <ctime>
-#include <fstream>
 #include <iostream>
-#include <stdio.h>
 #include <opencv2/opencv.hpp>
 #include <opencv/highgui.h>
 
@@ -15,14 +12,13 @@ using namespace std;
 using namespace cv;
 
 int WS_InitCamera(RaspiCam_Cv *Camera){
-	if(DEBUG)cout<<"\tWS_InitCamera: Setting format of output data to CV_8UC1"<<endl;
-	Camera->set(CV_CAP_PROP_FORMAT, CV_8UC1); //CV_8UC1 seems to be standard.
+	if(DEBUG)cout<<"\tWS_InitCamera: Setting format of output data to CV_8UC3"<<endl;
+	Camera->set(CV_CAP_PROP_FORMAT, CV_8UC3); //CV_8UC3 seems to be standard. CV_8UC1 return black and white images (one channels instead of three).
 	if(DEBUG)cout<<"\tWS_InitCamera: Setting FPS to "<<FPS<<endl;
 	Camera->set(CV_CAP_PROP_FPS, FPS);
 	if(DEBUG)cout<<"\tWS_InitCamera: Setting frame dimensions to "<<WIDTH<<"x"<<HEIGHT<<endl;
 	Camera->set(CV_CAP_PROP_FRAME_WIDTH, WIDTH);
 	Camera->set(CV_CAP_PROP_FRAME_HEIGHT, HEIGHT);
-
 	if(DEBUG)cout<<"\tWS_InitCamera: Opening camera... "<<Camera->open()<<endl;
 
 	return 0;
