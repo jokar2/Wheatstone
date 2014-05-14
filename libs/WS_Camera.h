d63729a5dff2a843f846babf955f382325d2980b
jokar2 <pi@raspberrypi.(none)>
2014-05-14 12:04:23 +0000

    Added the functions WS_Motion_Detection and WS_RecordAndSend

--- libs/WS_Camera.h
+++ libs/WS_Camera.h
@@ -5,6 +5,7 @@
 
 using namespace raspicam;
 
-int WS_InitCamera(RaspiCam_Cv *Camera); //Initiates and configures the Raspberry Pi camera input  
-
+int WS_InitCamera(RaspiCam_Cv *Camera,int setup); //Initiates the camera module with settings if "setup" is 1. Opens camera.
+int WS_Motion_Detection(RaspiCam_Cv *Camera, int sensitivity); //When motion is detected the function returns 1. The variable "sensitivity" determines how sensitive the motion detection should be.
+void WS_RecordAndSend(RaspiCam_Cv *Camera); //Starts the script file ./test.sh that should record with raspivid
 #endif
